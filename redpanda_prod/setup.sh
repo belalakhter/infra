a2cc5de4f960e9a40312f69c2ebe86cde1312623
Bilal Akhtar <nobodyneo666@gmail.com>
2025-06-28 12:39:29 +0500

    c

--- redpanda_prod/setup.sh
+++ redpanda_prod/setup.sh
@@ -3,16 +3,28 @@ set -e
 
 command -v python3 >/dev/null || { echo "Python3 not found."; exit 1; }
 
-command -v pipx >/dev/null || {
-  python3 -m ensurepip --upgrade
+if ! command -v pip3 >/dev/null; then
+  echo "Installing pip via get-pip.py..."
+  curl -sSL https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+  python3 get-pip.py --user
+  rm -f get-pip.py
+  export PATH="$HOME/.local/bin:$PATH"
+fi
+
+if ! command -v pipx >/dev/null; then
+  echo "Installing pipx..."
   python3 -m pip install --user pipx
   export PATH="$HOME/.local/bin:$PATH"
   python3 -m pipx ensurepath
-}
+fi
 
 export PATH="$HOME/.local/bin:$PATH"
 
-command -v ansible-playbook >/dev/null || pipx install --include-deps ansible
+
+if ! command -v ansible-playbook >/dev/null; then
+  echo "Installing Ansible via pipx..."
+  pipx install --include-deps ansible
+fi
 
 ansible-playbook -i inventory setup_k3s.yaml
 ansible-playbook -i inventory setup_redpanda.yaml
