54850055dff05b3636257d09c75f7a053d737172
Bilal Akhtar <nobodyneo666@gmail.com>
2025-06-28 13:43:45 +0500

    c

--- redpanda_prod/setup.sh
+++ redpanda_prod/setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 
-command -v python3 >/dev/null || { echo "Python3 not found."; exit 1; }
-
+export LANG=C.UTF-8
+export LC_ALL=C.UTF-8
 export PATH="$HOME/.local/bin:$PATH"
 
+command -v python3 >/dev/null || { echo "Python3 not found."; exit 1; }
 
 if ! command -v ansible-playbook >/dev/null; then
   echo "Installing Ansible via pipx..."
